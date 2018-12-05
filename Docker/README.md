14b22d41edffedb4556f682b6e6c469d29574462
fantasyqt <fanqtch@gmail.com>
2018-12-05 06:46:49 -0800

    update dockerfile and add readme.md

---
+++ Docker/README.md
@@ -0,0 +1,15 @@
+# how to use 
+
+```
+$ sudo docker build -t hctf:test .
+
+$ sudo docker run -dt -p 2000:3000  hctf:test bash
+```
+
+and then you should exec in your docker and run ` /run.sh `
+
+
+# admin 
+
+username:admin@admin.com
+password:admina
--- Docker/run.sh
+++ Docker/run.sh
@@ -4,5 +4,8 @@ mysql -uroot -e "SET GLOBAL INNODB_LARGE_PREFIX = ON;SET GLOBAL innodb_file_form
 su rails <<EOF 
 RAILS_ENV=production bundle exec rake db:migrate;
 nohup bin/rails s -e production &
-exit
+ls;
+exit;
 EOF
+mysql -uroot -e "insert into hctf.users(id,email,encrypted_password,created_at,updated_at,role) values(1,'admin@admin.com','\$2a\$11\$1q/KBj1gGUYAQVndWuPPb.zm6QzYr9P2SjdWdarUFFX7WyqNn19/a','2018-12-05 14:10:10.055691','2018-12-05 14:10:10.055691','admin');"
+
